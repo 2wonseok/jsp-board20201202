9488d1e1ebe3a581cb1d75f9f3bd3874c34b714f
2wonseok <lws3793@naver.com>
2020-12-08 19:30:24 +0900

    12/08 mvc

--- WebContent/WEB-INF/view/null.jsp
+++ WebContent/WEB-INF/view/null.jsp
@@ -1,9 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<%@ page import="java.util.*" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<%@ taglib prefix="my" tagdir="/WEB-INF/tags/lecture" %>
-<% request.setCharacterEncoding("UTF-8"); %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -15,6 +12,8 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+<div class="container">
+	<h1>잘못된 경로입니다.</h1>
+</div>
 </body>
 </html>
---
+++ src/jdbc/JDBCInitListener.java
@@ -0,0 +1,71 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ * Application Lifecycle Listener implementation class JDBCInitListener
+ *
+ */
+@WebListener
+public class JDBCInitListener implements ServletContextListener {
+
+    /**
+     * Default constructor. 
+     */
+    public JDBCInitListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent sce)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent sce)  { 
+    	ServletContext application = sce.getServletContext();
+    	
+    	String url = application.getInitParameter("jdbcUrl");
+    	String user = application.getInitParameter("jdbcUser");
+    	String pw = application.getInitParameter("jdbcPassword");
+
+    	
+    	// 1. 클래스 로딩
+    	try {
+				Class.forName("oracle.jdbc.driver.OracleDriver");
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	
+    	// 2. drivermanager에서 connection
+    	// 3. close();
+//			try {
+//				Connection con = DriverManager.getConnection(url, user, pw);
+//			} catch (Exception e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+    	
+    	ConnectionProvider.setUrl(url);
+    	ConnectionProvider.setUser(user);
+    	ConnectionProvider.setPassword(pw);
+			
+//    	Connection con = ConnectionProvider.getConnection();
+    	// context root 경로
+    	String contestPath = application.getContextPath();
+    	application.setAttribute("root", contestPath);
+    }
+	
+}
