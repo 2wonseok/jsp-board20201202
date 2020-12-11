af9596a61443107a5dd0d1fe078753ba43d9380e
2wonseok <lws3793@naver.com>
2020-12-11 18:57:56 +0900

    게시판 생성 쿼리

--- WebContent/WEB-INF/sql/myboard.sql
+++ WebContent/WEB-INF/sql/myboard.sql
@@ -7,14 +7,15 @@ CREATE TABLE member (
 
 SELECT * FROM member;
 
-CREATE TABLE artice (
+DROP TABLE article;
+CREATE TABLE article (
     article_no NUMBER GENERATED AS IDENTITY,
     writer_id VARCHAR2(50) NOT NULL,
     writer_name VARCHAR2(50) NOT NULL,
     title VARCHAR2(255) NOT NULL,
     regdate DATE NOT NULL,
     moddate DATE NOT NULL,
-    read_cnd NUMBER,
+    read_cnt NUMBER,
     PRIMARY KEY (article_no)
 );
 
@@ -26,3 +27,37 @@ CREATE TABLE article_content (
 );
 
 SELECT * FROM article_content;
+
+SELECT * FROM article
+WHERE ROWNUM =1;
+
+ALTER TABLE artice
+RENAME TO article;
+
+SELECT * FROM (SELECT * FROM member ORDER BY memberid DESC) WHERE ROWNUM = 1;
+
+SELECT * FROM (SELECT * FROM article ORDER BY writer_id DESC) WHERE ROWNUM = 1;
+SELECT * FROM article ORDER BY article_no;
+SELECT * FROM article_content;
+SELECT COUNT(*) FROM article;
+
+INSERT INTO article (writer_id, writer_name, title, regdate, moddate, read_cnt) 
+VALUES ('b' , 'b' , 'b' , SYSDATE, SYSDATE, 0);
+
+commit;
+
+SELECT * FROM article ORDER BY article_no DESC;
+
+SELECT * FROM (SELECT article_no, title, ROW_NUMBER() 
+                OVER (ORDER BY article_no DESC) rn 
+                FROM article)
+WHERE rn BETWEEN 1 AND 5;
+
+SELECT rn, article_no, writer_id, writer_name, title, regdate, moddate, read_cnt FROM (SELECT article_no, writer_id, writer_name, title, regdate, moddate, read_cnt, ROW_NUMBER() OVER (ORDER BY article_no DESC) rn FROM article) WHERE rn  BETWEEN 1 AND 5;
+
+SELECT * FROM (SELECT article_no, writer_id, writer_name, title, regdate, moddate, read_cnt, ROW_NUMBER() OVER (ORDER BY article_no DESC) rn FROM article) WHERE rn  BETWEEN 1 AND 5;
+
+SELECT * FROM article ORDER BY article_no DESC OFFSET 6 ROWS FETCH FIRST 10 ROWS ONLY;
+
+SELECT COUNT(*) FROM article;
+SELECT ROW_NUMBER() OVER (ORDER BY article_no DESC) rs FROM article;
